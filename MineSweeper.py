2c06f2d4a6ee410ec52a39cea28d7f1eff789acb
Timothy Pearman <148655506+Timmy1404@users.noreply.github.com>
2024-07-12 19:04:59 +0100

    Add files via upload
    got pop up window working

---
+++ MineSweeper.py
@@ -0,0 +1,13 @@
+import pygame
+
+#initalise the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((500,500))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
