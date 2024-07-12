60a1ad2b26ed9c7217041f33f7f416f2f566b4f6
Timothy Pearman <148655506+Timmy1404@users.noreply.github.com>
2024-07-12 19:40:17 +0100

    Update MineSweeper.py
    added icon, name and background colour

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,13 +1,33 @@
 import pygame
 
-#initalise the pygame
-pygame.init()
+# initializing the constructor 
+pygame.init() 
+  
+# screen resolution 
+res = (500,500) 
+# opens up a window 
+screen = pygame.display.set_mode(res) 
+  
+# stores the dimentions of the screen into a variables
+width = screen.get_width() 
+height = screen.get_height() 
 
-#create the screen
-screen = pygame.display.set_mode((500,500))
+#title and icon
+pygame.display.set_caption("Mine Sweeper")
+icon = pygame.image.load('bomb.png')
+pygame.display.set_icon(icon)
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+#game loop
+while True: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            pygame.quit() 
+
+    #background RGB values
+    screen.fill((128,128,128))
+    
+    #update any changes onto the window screen
+    pygame.display.update()
+
+            
+        
